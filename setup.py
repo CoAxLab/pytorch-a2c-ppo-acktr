2ebc4da3dbf34e638a9e6718b8c0929b6bab4bbd
Erik Peterson <Erik.Exists@gmail.com>
2018-09-12 15:42:06 -0700

    Tweaked loc of run_a2c_ppo_acktr

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name='a2c-ppo-acktr',
     packages=find_packages(),
     version='0.0.1',
-    scripts=['a2c_ppo_acktr/run_a2c_ppo_acktr.py'],
+    scripts=['run_a2c_ppo_acktr.py'],
     install_requires=['gym', 'matplotlib', 'pybullet'])
